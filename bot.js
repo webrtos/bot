e2c49f5a7a6dfd40711d1b4008e4313c195104d4
webrtos <admin@webrtos.com>
2023-01-29 13:00:33 +0200

    listening

--- bot.js
+++ bot.js
@@ -96,7 +96,8 @@ async function createCommand() {
 
 //export default function () {
     app.listen(process.env.PORT || 3000, function () {
-        console.log('Listening on', this.address());
+        console.log('Listening...');
+        //console.log('Listening on', this.address());
         createCommand();
     });
 //}
